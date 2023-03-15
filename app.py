adfdffa82e5277eb5653f0223ab5e57797f9c0a4
kouissar <kouissar@gmail.com>
2023-03-14 23:32:56 -0400

    first major commit

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import datetime
+from datetime import date
 
 
 
@@ -13,6 +16,28 @@ st.set_page_config(
 
 
 # local_css("style/style.css")
+data_file = 'data/data.csv'
+def get_data_from_csv(path):
+    df = pd.read_csv(path, header=0, sep=',')
+    return df
 
+df = get_data_from_csv(data_file)
+now = datetime.datetime.now()
+today = now.strftime("%Y-%m-%d %H:%M:%S")
+#sidebar
+st.sidebar.subheader("Enter a new activity")
+activity = st.sidebar.selectbox('Select the activity', options=['Wim Hof Breathing', '4-7-8 Breathing', 'Cold Shower', '10 mins Meditation', 'Walk'])
+duration = st.sidebar.text_input('Enter a duration', 0)
 
-st.write("hello world")
+click_submit = st.sidebar.button("Submit")
+st.title(':palm_tree: Mindfullness Tracker')
+if click_submit == True:
+    d = {'Date': today,
+         'Activity': activity,
+         'Duration': duration
+         }
+    st.markdown('<h3>Thank you for your feedback!</h3>', unsafe_allow_html=True)
+    df = df.append(d, ignore_index = True)
+    df.to_csv(data_file, index=False, header=True)
+
+st.dataframe(df)
