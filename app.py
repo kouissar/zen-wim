a845aa59fd0a8d4a81b5b22dca0b1a1b015ec848
kouissar <kouissar@gmail.com>
2023-03-14 23:07:46 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+
+st.set_page_config(
+    page_title="My Digital Zen World", layout="wide"
+)
+
+# # Use local CSS
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+# local_css("style/style.css")
+
+
+st.write("hello world")
